cc767830a66e010cb32e6572c0f5ace46fc6277d
mamoudoub3 <mamoudou.bah.mb3@gmail.com>
2023-11-03 20:16:55 -0400

    first commit

---
+++ eclipse-workspace/ZZZZZZZ_GitDemoProject/src/Test1.java
@@ -0,0 +1,12 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry = "hungry";
+		if (hungry == "yes" ){
+			System.out.println("eat something....");
+		} else {
+			System.out.println("Do some owrk....");
+		}
+	}
+}
